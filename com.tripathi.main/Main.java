1ef24eb6f5393c9487bd35784786b3520d0a47c3
Saurabh Tripathi <st954455@gmail.com>
2024-07-22 01:24:10 +0530

    My first Program

---
+++ com.tripathi.main/Main.java
@@ -0,0 +1,16 @@
+package com.tripathi.main;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] numbers = {12,15,18,20};
+        System.out.println("Hello World");
+        int i=1;
+        for(int num:numbers)
+        {
+            System.out.println("Number at "+i+" Postion is :"+num);
+            i++;
+        }
+
+    }
+}
